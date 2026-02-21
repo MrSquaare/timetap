41d6b58099fc7f2b44b835c65f5374e2298c83d8
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2026-02-21 17:42:39 +0100

    fix(lib/database): enable sqlite foreign key
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,5 +3,9 @@ import { openDatabaseSync } from "expo-sqlite";
 
 import migrations from "../../drizzle/migrations";
 
-export const database = drizzle(openDatabaseSync("db.db"));
+export const sqliteDatabase = openDatabaseSync("db.db");
+
+sqliteDatabase.execSync("PRAGMA foreign_keys = ON");
+
+export const database = drizzle(sqliteDatabase);
 export { migrations };
