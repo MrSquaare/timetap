90aaf3c62e2e527c9ed3e8cb4987f2ee34bfcb41
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2026-02-22 21:06:43 +0100

    feat: enable react compiler
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "eslint/config";
 import expo from "eslint-config-expo/flat.js";
 import prettier from "eslint-plugin-prettier/recommended";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default defineConfig(
   expo,
+  reactCompiler.configs.recommended,
   prettier,
   {
     rules: {
